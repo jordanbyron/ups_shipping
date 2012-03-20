399f8f28f06f23dc514ccd773b0027a4d28d95bd
Jordan Byron <jordan.byron@gmail.com>
2012-03-20 16:09:51 -0400

    Fix up void shipments

--- lib/ups_shipping/void_shipment_request.rb
+++ lib/ups_shipping/void_shipment_request.rb
@@ -1,41 +1,34 @@
 module UpsShipping
   class VoidShipmentRequest < Request
     attr_accessor :tracking_number
-    attr_accessor :production_override
-    
-    def initialize(tracking_number, production_override = false)
+
+    def initialize(tracking_number)
       self.tracking_number = tracking_number
-      self.production_override = production_override
     end
-    
-    def request_uri
-      production_override ? URI.parse("https://www.ups.com/ups.app/xml/" + path_suffix) : super
-    end
-    
+
     def path_suffix
       "Void"
     end
-    
+
     def body
       xml = Builder::XmlMarkup.new(:indent => 2)
-      
+
       build_credentials(xml)
-      
+
       xml.instruct!(:xml, :encoding => "UTF-8")
       xml.VoidShipmentRequest do
         xml.Request do
           xml.TransactionReference do
+            # Custom attribute which is returned by the server
             xml.CustomerContext
-            xml.XpciVersion
           end
-          
+
           xml.RequestAction 1
-          xml.RequestOption 1
         end
-        
+
         xml.ShipmentIdentificationNumber tracking_number
       end
     end
-    
+
   end
 end
--- lib/ups_shipping/void_shipment_response.rb
+++ lib/ups_shipping/void_shipment_response.rb
@@ -1,15 +1,26 @@
 module UpsShipping
   class VoidShipmentResponse < Response
+    attr_accessor :voided, :tracking_number
+
+    def voided?
+      self.voided == '1'
+    end
+
     def parse_response
-      data = Hash.from_xml(body)["void_shipment_response"]
-      
-      self.status_code = data['response']['response_status_code']
-      self.status_description = data['response']['response_status_description']
-      
-      unless success?
-        self.error_severity = data['response']['error']['error_severity']
-        self.error_code = data['response']['error']['error_code']
-        self.error_description = data['response']['error']['error_description']
+      self.xml = Nokogiri::XML.parse(body)
+
+      self.status_code        = attr('ResponseStatusCode')
+      self.status_description = attr('ResponseStatusDescription')
+
+      if success?
+        self.tracking_number = attr("TrackingNumber")
+        self.voided          = attr("PackageLevelResults/StatusCode") == '1'
+      else
+        self.error_severity    = attr("ErrorSeverity")
+        self.error_code        = attr("ErrorCode")
+        self.error_description = attr("ErrorDescription")
+
+        self.voided = false
       end
     end
   end
